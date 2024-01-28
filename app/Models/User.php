2a177b5de521e05d8e46feeb605dfc2b165c5828
Jose Daniel De La Cruz Rivera <drivera@doto.com.mx>
2024-01-28 00:36:49 -0600

    docker sail

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Eloquent\Model;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $connection = 'mongodb';
 
     /**
      * The attributes that are mass assignable.
